49db85ca9b95c60648f3c0c0c5b236e37120ab8b
A3 <adrie@konfidence.nl>
2021-12-28 16:32:59 +0100

    setup home page, setup first part of pageissurelist

--- iDealSample3.1/Controllers/HomeController.cs
+++ iDealSample3.1/Controllers/HomeController.cs
@@ -1,6 +1,10 @@
-﻿using System.Diagnostics;
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
 using iDealSampleCore.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
 namespace iDealSampleCore.Controllers
@@ -9,9 +13,13 @@ namespace iDealSampleCore.Controllers
     {
         private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+
+        private readonly IConfiguration _configuration;
+
+        public HomeController(ILogger<HomeController> logger, IConfiguration configuration)
         {
             _logger = logger;
+            _configuration = configuration;
         }
 
         public IActionResult Index()
@@ -19,9 +27,48 @@ namespace iDealSampleCore.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
+        public IActionResult PageIssuerList()
         {
-            return View();
+            var pageIssuerListModel = new PageIssuerListModel
+            {
+                AcquirerUrl = _configuration["AcquirerUrl"],
+                MerchantId = _configuration["MerchantId"],
+                SubId = _configuration["SubId"],
+                DateTime = null
+            };
+
+            pageIssuerListModel.DropDownListIssuers = new SelectList(Enumerable.Empty<string>());
+
+            return View(pageIssuerListModel);
+        }
+
+        public IActionResult GetIssuerList()
+        {
+            var pageIssuerListModel = new PageIssuerListModel
+            {
+                AcquirerUrl = _configuration["AcquirerUrl"],
+                MerchantId = _configuration["MerchantId"],
+                SubId = _configuration["SubId"],
+                DateTime = DateTime.Now
+            };
+
+            pageIssuerListModel.DropDownListIssuers = new SelectList(Enumerable.Empty<string>());
+            ViewData["Issuers"] = new SelectList(Enumerable.Empty<string>());
+
+            return View("PageIssuerList", pageIssuerListModel);
+        }
+
+        public IActionResult TransActionRequest()
+        {
+            var pageIssuerListModel = new PageIssuerListModel
+            {
+                AcquirerUrl = _configuration["AcquirerUrl"],
+                MerchantId = _configuration["MerchantId"],
+                SubId = _configuration["SubId"],
+                DateTime = null
+            };
+
+            return View("PageIssuerList", pageIssuerListModel);
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
---
+++ iDealSample3.1/Models/PageIssuerListModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace iDealSampleCore.Models
+{
+    public class PageIssuerListModel
+    {
+        [Display(Name = "Merchant Id:")]
+        public string MerchantId { get; set; }
+
+        [Display(Name = "Sub Id:")]
+        public string SubId { get; set; }
+        
+        [Display(Name = "Acquirer Url:")]
+        public string AcquirerUrl { get; set; }
+
+        [Display(Name = "DateTime:")]
+        public DateTime? DateTime { get; set; }
+
+        [Display(Name = "Issuer List:")]
+        public IEnumerable<SelectListItem> DropDownListIssuers { get; set; }
+    }
+}
